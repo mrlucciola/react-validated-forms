b9c8ca99daf0484e618512b7e5f7f1e19884807c
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2025-06-17 17:31:21 -0400

    feat: create type AnyCvCb

--- src/utils/formConfigTypes.ts
+++ src/utils/formConfigTypes.ts
@@ -3,10 +3,16 @@ import type { EvOut, FormOut } from "./formOutputTypes";
 /** Represents "Calculated Values Callback"
  * Input form values + (optional) external values and returns `calculated` values
  */
-export type CvCb<TFv extends FormOut, TEv extends EvOut, TCv> = (
+export type CvCb<TFv extends FormOut, TCv, TEv extends EvOut> = (
   form: TFv,
   externalValues?: TEv
 ) => TCv;
 
+export type AnyCvCb<TFv extends FormOut = FormOut, TCv = any, TEv extends EvOut = any> = CvCb<
+  TFv,
+  TCv,
+  TEv
+>;
+
 /** "Calculated Values Callback" type, derived from "calculatedValues" */
-export type CvCbFromCv<TCv> = TCv extends never | undefined ? never : CvCb<any, any, TCv>;
+export type CvCbFromCv<TCv> = TCv extends never | undefined ? never : CvCb<any, TCv, any>;
