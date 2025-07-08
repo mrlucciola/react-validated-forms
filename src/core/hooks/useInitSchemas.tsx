0452d3cdd88c09931980106b8c2d159a9f248ddf
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2025-07-08 14:10:28 -0400

    refactor: reduce scope of coersion within useInitSchemas and remove param

--- src/core/hooks/useInitSchemas.tsx
+++ src/core/hooks/useInitSchemas.tsx
@@ -5,9 +5,9 @@ import { buildDefaultSchema } from "@core/utils";
 // interfaces
 import type { CalcValuesOpt, ZObj, ZObjOpt } from "@utils/rootTypes";
 import type { ExtSchema, UiSchema } from "@utils/schemaTypes";
-import type { CvCbDefinition } from "@utils/configPropTypes";
 import type { ResolveFor, ResolveProp } from "@utils/utilityTypes";
 import type { InitSchemas } from "@core/hooks/interfaces";
+import type { ConfigDef } from "@utils/configTypes";
 
 type Schemas<TFs extends ZObj, TEs extends ZObjOpt> = {
   /** The schema used for validating values used outside of the form fields.
@@ -28,11 +28,9 @@ type Schemas<TFs extends ZObj, TEs extends ZObjOpt> = {
    */
 } & ResolveProp<ResolveFor<ExtSchema<TEs>, ZObj>, "evSchema">;
 
-const useInitSchemas = <TFs extends ZObj, TEs extends ZObjOpt, TCv extends CalcValuesOpt>(config: {
-  schema: TFs;
-  externalSchema?: TEs;
-  calcValuesCallback?: CvCbDefinition<TFs, TEs, TCv>;
-}): Schemas<TFs, TEs> => {
+const useInitSchemas = <TFs extends ZObj, TEs extends ZObjOpt, TCv extends CalcValuesOpt>(
+  config: Pick<ConfigDef<TFs, TEs, TCv, any>, "schema" | "externalSchema">
+): Schemas<TFs, TEs> => {
   if (config.schema instanceof z.ZodEffects) {
     throw new Error(
       `Schema cannot be an Effect type (preprocess/transform/refine): Provided: ${config.schema}`
@@ -55,12 +53,15 @@ const useInitSchemas = <TFs extends ZObj, TEs extends ZObjOpt, TCv extends CalcV
    * @note The output values of this schema should only be used within the form components.
    */
   const uiSchema: UiSchema<TFs> = useMemo(() => buildDefaultSchema(baseSchema), []);
-  const evSchema = useMemo(() => {
-    const out = config.externalSchema ? buildDefaultSchema(config.externalSchema) : undefined;
-    return out;
-  }, []) as ExtSchema<TEs>; // @todo remove this coersion
 
   const out: InitSchemas<TFs, TEs> = { baseSchema, uiSchema };
+
+  const evSchema = useMemo(() => {
+    if (config.externalSchema) {
+      return buildDefaultSchema(config.externalSchema) as ExtSchema<TEs>; // @todo remove this coersion
+    }
+    return undefined;
+  }, []);
   if (config.externalSchema) {
     out.evSchema = evSchema;
   }
